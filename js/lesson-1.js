df11155b1ab75fd2499929f4737e922a5482f018
Oleh Shyian <shiyan4242@gmail.com>
2024-07-20 13:39:37 +0200

    fix

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,1 +1,19 @@
-console.log(`Hellow world`);
+console.log(`Hello ${}`);
+console.log();
+console.log('object :>> ', object);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+if (condition) {
+    
+}
